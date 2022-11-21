6096b2f062d8fa7fa8645b27b38d6355ac947d87
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-11-21 13:39:01 +0900

    Create fuck-fuck-devtools.user.js

---
+++ fuck-fuck-devtools.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Fuck Fuck devtools
+// @namespace    https://github.com/typeling1578/Fuck-Fuck-devtools
+// @version      0.1
+// @description  Fuck Fuck devtools
+// @author       typeling1578
+// @match        *://*/*
+// @icon         none
+// @grant        none
+// @run-at document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let n6LRZjuNuSJeA3 = Object.assign({}, console);
+
+    console.log = function(...aq64ZZsEmW3qxD) {
+        for (let YrLhEgo9VawXU4 of aq64ZZsEmW3qxD) {
+            YrLhEgo9VawXU4.toString = "".toString;
+        }
+        n6LRZjuNuSJeA3.log(...aq64ZZsEmW3qxD);
+    }
+})();
